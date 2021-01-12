5e6b85cb4ba608924fb897834375036f2c9bcb93
Efuxx <artur.x.x.z505@gmail.com>
2021-01-12 21:07:55 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dddd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
